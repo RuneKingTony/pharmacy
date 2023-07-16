dfd944bb9c86c2399cbb2c839fc74b0cc226f3d1
GabrielNkwa <gnkwa@outlook.com>
2023-07-16 14:59:42 +0100

    gallery and board

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import About from './About'
-import Board from './Board'
-import Facts from './Facts'
-import Footer from './Footer'
-import Gallery from './Gallery'
-import Hero from './Hero'
-import Navbar from './Navbar'
-import Vision from './Vision'
-
+import React from 'react';
+import About from './About';
+// import Board from './Board'
+import Facts from './Facts';
+import Footer from './Footer';
+// import Gallery from './Gallery'
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Vision from './Vision';
 
 const Layout = () => {
   return (
-    <div className='layout'>
-     
-        <title>Tony Dev Store</title>
-      
+    <div className="layout">
+      <title>Tony Dev Store</title>
 
-       <Navbar/>
-       <Hero/>
-       <Gallery/>
-       <About/>
-      <Vision/>
-       <Facts/>
-       <Board/>
-     <Footer/>
+      <Navbar />
+      <Hero />
+      {/* <Gallery/> */}
+      <About />
+      <Vision />
+      <Facts />
+      {/* <Board/> */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
